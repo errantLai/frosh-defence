cd91766af46362b752cdc6a58bce040c16db48a9
crowe <15cdc2@caslab.queensu.ca>
2018-11-13 10:18:03 -0500

    Added GameController comments
    Added more comments to GameController header and implementation files.

--- src/GameController.cpp
+++ src/GameController.cpp
@@ -22,25 +22,25 @@ sf::RenderWindow window(sf::VideoMode(1920,1080), "Hello World");
 	GameBoard::GameBoard(){}
 	void GameBoard::initBoard(){
 
-		// Create the gameboard
+		// Grass Tile Objects
 		grassTile = sf::RectangleShape(sf::Vector2f(60, 60));
 		grassTile.setFillColor(sf::Color(100, 250, 50)); // green
 		grassTile.setOutlineThickness(-1);
 		grassTile.setOutlineColor(sf::Color(0, 0, 0, 50));
-
+		// Ground Tile Objects
 		groundTile = sf::RectangleShape(sf::Vector2f(60, 60));
 		groundTile.setFillColor(sf::Color(153, 140, 85)); // brown
 		groundTile.setOutlineThickness(-1);
 		groundTile.setOutlineColor(sf::Color(0, 0, 0, 100));
-
+		// Hover Object
 		hoverOutline = sf::RectangleShape(sf::Vector2f(60,60));
 		hoverOutline.setFillColor(sf::Color::Transparent);
 		hoverOutline.setOutlineColor(sf::Color::Red);
 		hoverOutline.setOutlineThickness(-3);
 	}
 
+	// Draws Map with Ground and Grass Objects
 	void GameBoard::renderBoard() {
-
 		int mapX=0, mapY=0;
 		// Each square is to be 60 pixels wide,
 		// with an exact height of 18 tiles
@@ -58,24 +58,25 @@ sf::RenderWindow window(sf::VideoMode(1920,1080), "Hello World");
 			Point next = path[i+1];		// Next Point
 			mapX= curr.getX();
 			mapY= curr.getY();
-			if(curr.getX() == next.getX()){			// If two points vertical
+			if(curr.getX() == next.getX()){				// If two points vertical
 				while(mapY!= next.getY()){
 					groundTile.setPosition(mapX* 60, mapY* 60);
 					window.draw(groundTile);
-					if(mapY < next.getY())mapY++;
+					if(mapY < next.getY())mapY++;		// Draw up or down until next point
 					else mapY--;
 				}
-			} else if(curr.getY() == next.getY()){	// If two points horizontal
+			} else if(curr.getY() == next.getY()){		// If two points horizontal
 				while(mapX!= next.getX()){
 					groundTile.setPosition(mapX* 60, mapY* 60);
 					window.draw(groundTile);
-					if(mapX < next.getX())mapX++;
+					if(mapX < next.getX())mapX++;		// Draw left or right until next point
 					else mapX--;
 				}
 			}
 		}
 	}
 
+	// Draw Hover Object
 	void GameBoard::renderHover(int mouseX, int mouseY, int range){
 		int gridX = ceil(mouseX/60);
 		int gridY = ceil(mouseY/60);
@@ -95,51 +96,54 @@ sf::RenderWindow window(sf::VideoMode(1920,1080), "Hello World");
 		froshRemaining = 100;
 		froshEliminated = 0;
 	}
-	int GameController::getHealth(){
-		return this->health;
-	}
-	int GameController::getTams(){
-		return this->tams;
-	}
-	int GameController::getCurrentWave(){
-		return this->currentWave;
-	}
-	int GameController::getFroshRemaining(){
-		return this->froshRemaining;
-	}
-	int GameController::getFroshEliminated(){
-		return this->froshEliminated;
-	}
+
+	// Game State Accessors
+	int GameController::getHealth(){ return this->health; }
+	int GameController::getTams(){ return this->tams; }
+	int GameController::getCurrentWave(){ return this->currentWave; }
+	int GameController::getFroshRemaining(){ return this->froshRemaining; }
+	int GameController::getFroshEliminated(){ return this->froshEliminated; }
+
+	// Start the Game
 	void GameController::startGame(){
 
 	}
+
+	// End Current Game
 	void GameController::endGame(){
 
 	}
+
+	// Start Next Wave
 	void GameController::startWave(){
 		this->currentWave++;
 	}
+
+	// Display Help Screen
 	void GameController::displayHelpScreen(){
 
 	}
+
+	// Update Player Lives
 	void GameController::updateHealth(int update){
 		this->health += update;
 	}
+
+	// Update Player Currency
 	void GameController::updateTam(int update){
 		this->tams += update;
 	}
 
+	// Point Class used for Map
 	Point::Point(int xIn, int yIn){
 		this->x = xIn;
 		this->y = yIn;
 	}
-	Point::getX(){
-		return this->x;
-	}
-	Point::getY(){
-		return this->y;
-	}
+	// Accessors
+	Point::getX(){ return this->x; }
+	Point::getY(){ return this->y; }
 
+	// Main
 	int main(){
 		sf::Font font;
 		if (!font.loadFromFile("arial.ttf")){}
