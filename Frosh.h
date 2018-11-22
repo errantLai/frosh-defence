0d3ac44aeaec1533d40c0714d7d98751261a7397
lai <13clff@caslab.queensu.ca>
2018-11-22 13:16:40 -0500

    Create frosh controller outline
    This is responsible for managing the state
    of all the frosh objects. This is just the header
    file, not the implementation.
    TBR-212

--- Frosh.h
+++ Frosh.h
@@ -11,39 +11,36 @@
 
 class Frosh {
 private:
+	sf::RectangleShape shape;
+	// The frosh controller will determine which specific frosh
+	// spritesheet to use. The frosh object is only responsible
+	// for changing specific directions, etc.
+	sf::Texture* texture;
+
 	// Internal game attributes
 	int tamValue;
 	int health;
 	// This determines how much health is lost when it crosses the finish
 	int damage;
 
-	sf::RectangleShape shape;
-	// The frosh controller will determine which specific frosh
-	// spritesheet to use. The frosh object is only responsible
-	// for changing specific directions, etc.
-	sf::Vector2f position;
-	sf::Vector2f size;
-	sf::Texture* texture;
-
-	// This assigns the relevant stats, with modifiers for later waves
-	void assignFroshStats(int type, double modifier);
 public:
 	// The actual texture definition occurs within the froshController
 	Frosh(sf::Vector2f position, sf::Vector2f size, sf::Texture* texture,
-			int type, double modifier);
+			int _tam, int _health, int _damage);
 	virtual ~Frosh();
+
+	// Game logic functions
+	void update();
+	void render(sf::RenderWindow* window);
 
 	// Accessors
 	int getTamValue();
 	int getHealth();
 	int getDamage();
 	sf::Vector2f getPosition();
+	void setPosition(sf::Vector2f position);
 	// This returns the resulting health of the frosh
 	int reduceHealth(int damage);
-
-	// Game logic functions
-	void update();
-	void render(sf::RenderWindow* window);
 };
 
 #endif /* FROSH_H_ */
