337916d47ba94f053f7d938a6326d5c37c130f0a
kinsella <15rklk@caslab.queensu.ca>
2018-11-24 18:03:53 -0500

    Cherry-pick core controller files
    TBR-252

---
+++ throwProjectile.h
@@ -0,0 +1,40 @@
+#pragma once
+//#include "ProjectileTypes.h"
+#include "SFML/Graphics.hpp"
+#include <iostream>
+#include "Frec.h"
+#include "Frosh.h"
+//#include "ProjectileTypes.h"
+#include <vector>
+
+
+class throwProjectile { //must extend frec class to access its position easiest/ most efficently 
+public:
+	throwProjectile(int ind, sf::Vector2f frecPosition, shared_ptr<Frosh> froshToFireAt);
+	~throwProjectile();
+	void drawProjectile(sf::RenderWindow &theWindow);
+	void moveObjectTowardsFrosh(sf::Vector2f frosh); //using frosh position, heatseeks towards the frosh until collision occurs
+	float DistanceFromFrosh(sf::Vector2f frosh); // returns distance between two points
+	bool projectileFroshCollision(sf::Vector2f frosh); // collision detection between two objects, using the distance function
+	void dealDamage(); //deals the damage associated with the projectile
+	sf::Vector2f normalize(sf::Vector2f v);
+	
+	//Accessors:
+	string getThrowProjImage();
+	sf::Vector2f getThrowProjectilePosition();
+	float getSpeed();
+	shared_ptr<Frosh> getFroshTarget();
+	int getDamage();
+	sf::Vector2f getProjectileCenter();
+
+private:
+	sf::Texture projectileTexture;
+	sf::RectangleShape projectileShape;
+	sf::Vector2f projectilePosition;
+	sf::Vector2f projectileCenter;
+	float speed;
+	int index;
+	int damage;
+	string throwProjImage = "assets/jacket.jpg";
+	shared_ptr<Frosh> froshTarget;
+};
