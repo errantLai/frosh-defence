d2f971b86edf636914e595ddef92f7affc555c77
kinsella <15rklk@caslab.queensu.ca>
2018-11-24 18:03:53 -0500

    Cherry-pick core controller files
    TBR-252

---
+++ FrecAndFroshController.cpp
@@ -0,0 +1,142 @@
+#include "FrecAndFroshController.h"
+FrecAndFroshController::FrecAndFroshController(int notUsed){
+	unused = notUsed; //must have private members to make an object
+}
+FrecAndFroshController::~FrecAndFroshController() {
+	for (auto &i : allThrowFrecs) { //same as for i < allThrowFrecs.size()
+		allThrowFrecs.erase(allThrowFrecs.begin(), allThrowFrecs.end());
+	}
+	//do for each special vector
+}
+
+/*______________________________________________THROW FREC_______________________________________________________*/
+
+void FrecAndFroshController::addThrowFrecToList(sf::Vector2f position, int index) {
+	allThrowFrecs.push_back(make_unique<Frec>(position, index));
+}
+void FrecAndFroshController::deleteThrowFrecAtIndex(int index) {
+	allThrowFrecs.erase(allThrowFrecs.begin() + index, allThrowFrecs.begin() + index + 1);
+	//delete image
+}
+void FrecAndFroshController::drawAllThrowFrecsOnGrid(sf::RenderWindow &theWindow) {//draws the frec onto the grid
+	for (int i=0; i <allThrowFrecs.size(); i++) { //for each throw frec
+		allThrowFrecs[i]->drawThrowFrec(theWindow);
+	}
+}
+/*______________________________________________THROW FREC_______________________________________________________*/
+
+/*___________________________________________OBJECT BEING THROWN_________________________________________________*/
+
+void FrecAndFroshController::addThrowObjectToList(int index, sf::Vector2f frecPosition, shared_ptr<Frosh> froshPtr) {
+	allThrowObjects.push_back(make_unique<throwProjectile>(index, frecPosition, froshPtr));
+}
+void FrecAndFroshController::deleteThrowObjectAtIndex(int index) {
+	allThrowObjects[index].reset();
+	allThrowObjects.erase(allThrowObjects.begin() + index, allThrowObjects.begin() + index + 1);
+	//delete image? I dont think you'll need to explicitly do this
+	//delete &allThrowObjects[index];
+}
+void FrecAndFroshController::drawAllThrowObjectsOnGrid(sf::RenderWindow &theWindow) {//draws the frec onto the grid
+	for (int i = 0; i < allThrowObjects.size(); i++) { //for each throw frec
+		allThrowObjects[i]->drawProjectile(theWindow);
+	}
+}
+/*__________________________________________OBJECT BEING THROWN_________________________________________________*/
+
+
+//_________Frosh Testing____________
+void FrecAndFroshController::drawAllFrosh(sf::RenderWindow &theWindow) {//draws the frec onto the grid
+	for (int i = 0; i < allFrosh.size(); i++) { //for each frosh
+		allFrosh[i]->render(&theWindow);
+	}
+}
+void FrecAndFroshController::addFroshObjectToList(sf::Vector2f _position, sf::Vector2f _size, sf::Texture* _texture,
+	int type, double modifier) {
+	allFrosh.push_back(make_unique<Frosh>(_position, _size, _texture, type, modifier));
+}
+//_________Frosh Testing____________
+
+
+
+
+
+
+/*___________________________________________Attack Logic_________________________________________________*/
+
+//if missile to frosh collision occurs, delete throwObject, else, move towards frosh
+void FrecAndFroshController::moveAllThrowObjectTowardsFroshAndDelete() { //this parameter will be replaced by allFrosh[j]->getPosition() in each if statement
+	for (int i = 0; i < allThrowObjects.size(); i++) { //for each throw object
+		//if there's a collision, delete object and deal damage
+		//cout<< "Projectile Positions:" << allThrowObjects[i]->getThrowProjectilePosition().x << " and y " << allThrowObjects[i]->getThrowProjectilePosition().y << endl;
+		
+		if (allThrowObjects[i]->projectileFroshCollision(allThrowObjects[i]->getFroshTarget()->getCenterPosition())) { 
+			//deal damage to frosh
+			allThrowObjects[i]->getFroshTarget()->reduceHealth(allThrowObjects[i]->getFroshTarget()->getHealth());
+			//getTams as well similar logic ^
+			cout << "DAMAGE DEALT, projectile deleted" << endl;
+			
+			//delete throw object
+			deleteThrowObjectAtIndex(i);
+		}
+		else{ //if the projectile assigned to a frosh hasnt hit yet, move towards frosh
+			allThrowObjects[i]->moveObjectTowardsFrosh(allThrowObjects[i]->getFroshTarget()->getCenterPosition());
+		}
+	}
+}
+
+//ascocitate each frec with a frosh within range, each with a vector of pointers (eg. frec index 0 will fire at frosh index 0)
+void FrecAndFroshController::updateFrecFroshRange() {
+	//clear contents of allThrowFrecsInRangeOfFrosh
+	if (allThrowFrecsInRangeOfFrosh.size() > 0)
+		allThrowFrecsInRangeOfFrosh.erase(allThrowFrecsInRangeOfFrosh.begin(), allThrowFrecsInRangeOfFrosh.end());
+	 //clear contents of 
+	if (allFroshInRangeOfFrecs.size() > 0)
+		allFroshInRangeOfFrecs.erase(allFroshInRangeOfFrecs.begin(), allFroshInRangeOfFrecs.end());
+	
+	for (int i = 0; i < allThrowFrecs.size(); i++) { //for each frec object
+		for (int j = 0; j < allFrosh.size(); j++) { //for each frosh object
+			if (allThrowFrecs[i]->CollidesWithFrec(allFrosh[j]->getPosition())) {
+				//cout << "Frec Frosh Collision" << endl;
+				allFroshInRangeOfFrecs.push_back(allFrosh[j]); //the indices will line up for each frec sticking to one frosh target
+				allThrowFrecsInRangeOfFrosh.push_back(allThrowFrecs[i]);
+			}
+		}
+	}
+}	
+
+
+
+
+//below creates two unique pointer vectors assigning each throw frec with a frosh target assigned to it
+//cooldown, a generation of projectiles
+//if frosh is in range of a frec, generate a projectile as long as the frec is not on cooldown
+//assign that projectile to a frosh object
+
+void FrecAndFroshController::process() { //essentially do every attack related function in a small time tick within process
+	//for each frec, decrease its cooldown by one tick, when at 0 it can fire
+	for (int i = 0; i < allThrowFrecs.size(); i++) {
+		if (allThrowFrecs[i]->getCooldown() > 1)
+			allThrowFrecs[i]->decreaseCoolDown();
+	}
+	//ascocitate each frec with a frosh within range, each with a vector of pointers (eg. frec index 0 will fire at frosh index 0)
+	updateFrecFroshRange();
+	//if the frec is not on cooldown, create a throwProjectile object that is assigned to a frosh object
+	for (int i = 0; i < allThrowFrecsInRangeOfFrosh.size(); i++) { //frecs and frosh will have same index i
+		if (allThrowFrecsInRangeOfFrosh[i]->getCooldown() == 0) { //if the frec is not on cooldown fire a new projectile, else do nothing
+			cout << "Firing Projectile" << endl;
+			allThrowFrecsInRangeOfFrosh[i]->resetCooldown();
+			addThrowObjectToList(allThrowObjects.size() - 1, allThrowFrecsInRangeOfFrosh[i]->getFrecPosition(), allFroshInRangeOfFrecs[i]); //append a throw object to the end of the vector
+			//allThrowObjects[allThrowObjects.size() - 1]->setFroshTarget(allFroshInRangeOfFrecs[i]); //assign a frosh target
+		}
+	}
+	//move each projectile towards its assigned frosh target, delete and deal damage if hit
+	moveAllThrowObjectTowardsFroshAndDelete();
+}
+
+
+void FrecAndFroshController::render(sf::RenderWindow &_window) {
+	drawAllThrowObjectsOnGrid(_window);
+	drawAllThrowFrecsOnGrid(_window);
+	drawAllFrosh(_window);
+}
+
---
+++ FrecAndFroshController.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <cstdio>
+#include <fstream>
+#include <cassert>
+#include <functional>
+#include "Frec.h"
+#include "throwProjectile.h"
+#include "Frosh.h"
+
+using namespace std;
+
+class FrecAndFroshController {
+public:
+	FrecAndFroshController(int notUsed);
+	~FrecAndFroshController();
+	//throwFrec Object
+	void addThrowFrecToList(sf::Vector2f position, int index); //using string
+	void deleteThrowFrecAtIndex(int index);
+	void drawAllThrowFrecsOnGrid(sf::RenderWindow &theWindow);
+	//Throw Objects
+	void addThrowObjectToList(int index, sf::Vector2f frecPosition, shared_ptr<Frosh> froshPtr);
+	void drawAllThrowObjectsOnGrid(sf::RenderWindow &theWindow);
+	void deleteThrowObjectAtIndex(int index);
+	void moveAllThrowObjectTowardsFroshAndDelete();
+	void updateFrecFroshRange();
+
+
+
+	//used for testing
+	void addFroshObjectToList(sf::Vector2f _position, sf::Vector2f _size, sf::Texture* _texture,
+		int type, double modifier);
+	void drawAllFrosh(sf::RenderWindow &theWindow);
+
+	void process();
+	void render(sf::RenderWindow &_window);
+private:
+	vector<unique_ptr<throwProjectile>> allThrowObjects;
+
+	vector<shared_ptr<Frec>> allThrowFrecs;
+	vector<shared_ptr<Frec>> allThrowFrecsInRangeOfFrosh;
+
+	vector<shared_ptr<Frosh>> allFroshInRangeOfFrecs;
+	vector<shared_ptr<Frosh>> allFrosh;
+	
+	//vector<unique_ptr<Frec>> allTSlamFrecs;
+//vector<unique_ptr<Frec>> allSkipFrecs;
+	//vector<unique_ptr<Frec>> allSlamObjects;
+//vector<unique_ptr<Frec>> allSkipObjects;
+	int unused;
+};
+
